9ae3c24af79749d6cc01f83ed1943c1d11292d7b
Moises <moisevic@hotmail.com>
2020-12-05 16:30:59 -0400

    Observable login and getUser

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
+import { ApiRestService } from "../services/api-rest.service"
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,26 +12,50 @@ export class LoginPage implements OnInit {
   formLogin: FormGroup;
   submitted = false;
   
-  constructor(public formBuilder: FormBuilder,) { }
-  
-  ngOnInit() {
-    this.formLogin = this.formBuilder.group({
-      usuario: ['', [Validators.required, Validators.minLength(4)]],
-      password: ['', [Validators.required, Validators.minLength(4)]],
-      remember: ['false'],
-    })
-  }
-  
-  onSubmit() {    
-    this.submitted = true;
-    if (!this.formLogin.valid) {
-      console.log('All fields are required.')
-      return false;
+  constructor(
+    public formBuilder: FormBuilder,
+    private apirest: ApiRestService,
+    ) { }
+    
+    ngOnInit() {
+      this.formLogin = this.formBuilder.group({
+        usuario: ['', [Validators.required, Validators.minLength(4)]],
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        remember: ['false'],
+      })
     }
-  }
+    
+    onSubmit() {    
+      this.submitted = true;
+      if (!this.formLogin.valid) {
+        console.log('All fields are required.')
+        return false;
+      }else{
+        this.apirest.login(this.formLogin.value)
+        .subscribe(
+          (response) => {
+            if (this.formLogin.value.remember == true) {
+              console.log(`Estas autorizado para realizar el login, y pidio recordar su usuario y clave. GUARDO LA INFO y llamo a GET USER `)
+              this.getUser(response);
+            }else{
+              console.log(`Estas autorizado para realizar el login, llamo a GET USER`);
+              this.getUser(response);
+            }
+          },
+          error => alert(`Debo llamar un modal con el error ${error.error.message}`),
+          )
+        }
+      }
+      get errorCtr() {
+        return this.formLogin.controls;
+      }
 
-  get errorCtr() {
-    return this.formLogin.controls;
-  }
-  
-}
+      getUser(data){
+        let token = `${data.token_type} ${data.access_token}`;
+        this.apirest.getUser(token, data.expires_at)
+        .subscribe(
+          (response)=>{console.log(response)},
+          error=>(alert(error))
+        );
+      }
+    }  
---
+++ src/app/services/api-rest.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiRestService {
+  urlAuth ='https://auradoc.bcnschool.net/api/auth/';
+  constructor(private http: HttpClient) { }
+
+  login(data: any): Observable<any> {
+
+    let headers = new HttpHeaders();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json');
+    headers.append('X-Requested-With', 'XMLHttpRequest');
+
+    const requestOptions = ({ headers: headers });
+    return this.http.post(`${this.urlAuth}login`, data, requestOptions);
+  }
+
+  getUser(authorization, expires_at): Observable<any> {
+    return this.http.get(`${this.urlAuth}user`, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'X-Requested-With': 'XMLHttpRequest', 'Authorization': authorization })
+    });
+  }
+
+  logout(): Observable<any> {
+    return this.http.get(`${this.urlAuth}+logout`);
+  }
+}
