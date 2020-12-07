12d996675e7a9e1ab2fb1410f82bba9fe00826f6
Moises <moisevic@hotmail.com>
2020-12-06 23:38:58 -0400

    Tabs Refresh Page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiRestService } from "../services/api-rest.service"
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -10,10 +11,30 @@ export class TabsPage {
   
   constructor(
     private apirest: ApiRestService,
+    private router: Router
     ) {}
     
+  ionViewWillEnter() {
+      this.router.navigated = false;
+      this.router.navigate([this.router.url]);
+  }
+ 
+
+  /*myDefaultMethodToFetchData() { 
+
+  }*/
+
     refresh(){
-      this.apirest.alertAcept('Actualizar', 'Se estará sincronizando su dispositivo, por favor espere.')
+      //this.apirest.alertAcept('Actualizar', 'Se estará sincronizando su dispositivo, por favor espere.')
+      this.ionViewWillEnter();
+      /*setTimeout(() => {
+        console.log("cargandooooooooo");
+        this.router.navigateByUrl(`tabs/tab1`);
+      }, 2000);*/
+
+      
+
+
     }
     
     exitApp() {
